59e378e36544cddb683db48c739e4a274a3b7269
Aadil313 <78995190+Aadil313@users.noreply.github.com>
2021-02-15 09:40:27 +0530

    Add files via upload

---
+++ as2q3.py
@@ -0,0 +1,14 @@
+a=[1,2,3,4,5]
+a.append(6)
+print(a)
+fruits =['apple','orange','banana']
+fruits.clear()
+print(fruits)
+b=['python','programe']
+b.insert(2, 'data')
+print(b)
+b.remove('python')
+print(b)
+
+
+
